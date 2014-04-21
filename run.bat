2a7f771380776ceb7703d6c0855d1d9de3f419d6
Jin Zhou <ferryzhou@gmail.com>
2014-04-21 09:57:21 -0400

    fixed del switch error

--- run.bat
+++ run.bat
@@ -1,11 +1,12 @@
 set indir=..\src
 set tmpdir=..\tmp
 if not exist %tmpdir% mkdir %tmpdir%
-del %tmpdir%\*.* /y
+del %tmpdir%\*.* /F /Q
+del status_all.pdf
 copy %indir%\*.docx %tmpdir%
 for %%f in (%tmpdir%\*.docx) do (
   echo %%f
-  OfficeToPDF %%f %%f.pdf
+  OfficeToPDF "%%f" "%%f.pdf"
 )
 
 pdftk %tmpdir%\*.pdf output status_all.pdf
